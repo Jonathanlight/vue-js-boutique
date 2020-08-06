6d445d1de5dadfc85f57b5ef6964f0d185dfe435
Jonathan <Jonathan.kablan@gmail.com>
2020-08-06 07:24:10 +0200

    header vue component

---
+++ boutique/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <TheHeader></TheHeader>
+  </div>
+</template>
+
+<script>
+import TheHeader from './components/TheHeader.vue';
+
+export default {
+  name: 'App',
+  components: {
+    TheHeader
+  }
+}
+</script>
+
+<style>
+</style>
